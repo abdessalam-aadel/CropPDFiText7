01644128e723be46d626861b3b9df00c7cdb8b2e
Abdessalam <48380521+abdessalam-aadel@users.noreply.github.com>
2024-09-04 16:05:59 +0100

    Add files via upload

---
+++ CropPDFiText7/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace CropPDFiText7
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            btnLoad = new Button();
+            txtBoxLoad = new TextBox();
+            btnStart = new Button();
+            lblDone = new Label();
+            lblAlert = new Label();
+            lblFileCount = new Label();
+            GitLink = new LinkLabel();
+            txtBoxX = new TextBox();
+            txtBoxY = new TextBox();
+            txtBoxWidth = new TextBox();
+            txtBoxHeight = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            IMGDone = new PictureBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)IMGDone).BeginInit();
+            SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            btnLoad.Cursor = Cursors.Hand;
+            btnLoad.Location = new Point(594, 12);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(102, 29);
+            btnLoad.TabIndex = 0;
+            btnLoad.Text = "Load ...";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // txtBoxLoad
+            // 
+            txtBoxLoad.Enabled = false;
+            txtBoxLoad.ForeColor = Color.Gray;
+            txtBoxLoad.Location = new Point(30, 14);
+            txtBoxLoad.Name = "txtBoxLoad";
+            txtBoxLoad.Size = new Size(546, 27);
+            txtBoxLoad.TabIndex = 1;
+            txtBoxLoad.Text = "Select your folder ...";
+            // 
+            // btnStart
+            // 
+            btnStart.Cursor = Cursors.Hand;
+            btnStart.Location = new Point(298, 306);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(94, 29);
+            btnStart.TabIndex = 2;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // lblDone
+            // 
+            lblDone.AutoSize = true;
+            lblDone.ForeColor = Color.Green;
+            lblDone.Location = new Point(572, 315);
+            lblDone.Name = "lblDone";
+            lblDone.Size = new Size(0, 20);
+            lblDone.TabIndex = 3;
+            // 
+            // lblAlert
+            // 
+            lblAlert.AutoSize = true;
+            lblAlert.ForeColor = Color.Red;
+            lblAlert.Location = new Point(30, 82);
+            lblAlert.Name = "lblAlert";
+            lblAlert.Size = new Size(0, 20);
+            lblAlert.TabIndex = 4;
+            // 
+            // lblFileCount
+            // 
+            lblFileCount.AutoSize = true;
+            lblFileCount.ForeColor = Color.Green;
+            lblFileCount.Location = new Point(30, 53);
+            lblFileCount.Name = "lblFileCount";
+            lblFileCount.Size = new Size(0, 20);
+            lblFileCount.TabIndex = 5;
+            // 
+            // GitLink
+            // 
+            GitLink.AutoSize = true;
+            GitLink.Cursor = Cursors.Hand;
+            GitLink.Location = new Point(311, 338);
+            GitLink.Name = "GitLink";
+            GitLink.Size = new Size(56, 20);
+            GitLink.TabIndex = 6;
+            GitLink.TabStop = true;
+            GitLink.Text = "GitHub";
+            GitLink.LinkClicked += GitLink_LinkClicked;
+            // 
+            // txtBoxX
+            // 
+            txtBoxX.Location = new Point(86, 119);
+            txtBoxX.Name = "txtBoxX";
+            txtBoxX.Size = new Size(125, 27);
+            txtBoxX.TabIndex = 7;
+            txtBoxX.Text = "580";
+            // 
+            // txtBoxY
+            // 
+            txtBoxY.Location = new Point(86, 152);
+            txtBoxY.Name = "txtBoxY";
+            txtBoxY.Size = new Size(125, 27);
+            txtBoxY.TabIndex = 8;
+            txtBoxY.Text = "0";
+            // 
+            // txtBoxWidth
+            // 
+            txtBoxWidth.Location = new Point(514, 119);
+            txtBoxWidth.Name = "txtBoxWidth";
+            txtBoxWidth.Size = new Size(125, 27);
+            txtBoxWidth.TabIndex = 9;
+            txtBoxWidth.Text = "793";
+            // 
+            // txtBoxHeight
+            // 
+            txtBoxHeight.Location = new Point(514, 152);
+            txtBoxHeight.Name = "txtBoxHeight";
+            txtBoxHeight.Size = new Size(125, 27);
+            txtBoxHeight.TabIndex = 10;
+            txtBoxHeight.Text = "1122";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 122);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 20);
+            label1.TabIndex = 11;
+            label1.Text = "X :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(30, 159);
+            label2.Name = "label2";
+            label2.Size = new Size(24, 20);
+            label2.TabIndex = 12;
+            label2.Text = "Y :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(431, 122);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 20);
+            label3.TabIndex = 13;
+            label3.Text = "Width :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(431, 155);
+            label4.Name = "label4";
+            label4.Size = new Size(61, 20);
+            label4.TabIndex = 14;
+            label4.Text = "Height :";
+            // 
+            // IMGDone
+            // 
+            IMGDone.Image = Properties.Resources.Done;
+            IMGDone.Location = new Point(623, 311);
+            IMGDone.Name = "IMGDone";
+            IMGDone.Size = new Size(25, 29);
+            IMGDone.SizeMode = PictureBoxSizeMode.StretchImage;
+            IMGDone.TabIndex = 15;
+            IMGDone.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Cursor = Cursors.Hand;
+            checkBox1.ForeColor = Color.Red;
+            checkBox1.Location = new Point(30, 219);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(282, 24);
+            checkBox1.TabIndex = 16;
+            checkBox1.Text = "Crop Yellow and green paper PDF file.";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Cursor = Cursors.Hand;
+            checkBox2.ForeColor = Color.Red;
+            checkBox2.Location = new Point(30, 249);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(139, 24);
+            checkBox2.TabIndex = 17;
+            checkBox2.Text = "Crop Bordereau.";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // frmMain
+            // 
+            AllowDrop = true;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(708, 380);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(IMGDone);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtBoxHeight);
+            Controls.Add(txtBoxWidth);
+            Controls.Add(txtBoxY);
+            Controls.Add(txtBoxX);
+            Controls.Add(GitLink);
+            Controls.Add(lblFileCount);
+            Controls.Add(lblAlert);
+            Controls.Add(lblDone);
+            Controls.Add(btnStart);
+            Controls.Add(txtBoxLoad);
+            Controls.Add(btnLoad);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "frmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Crop PDF using iText7";
+            DragDrop += frmMain_DragDrop;
+            DragEnter += frmMain_DragEnter;
+            ((System.ComponentModel.ISupportInitialize)IMGDone).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnLoad;
+        private TextBox txtBoxLoad;
+        private Button btnStart;
+        private Label lblDone;
+        private Label lblAlert;
+        private Label lblFileCount;
+        private LinkLabel GitLink;
+        private TextBox txtBoxX;
+        private TextBox txtBoxY;
+        private TextBox txtBoxWidth;
+        private TextBox txtBoxHeight;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private PictureBox IMGDone;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+    }
+}
---
+++ CropPDFiText7/Form1.cs
@@ -0,0 +1,292 @@
+using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using System.Diagnostics;
+
+namespace CropPDFiText7
+{
+    public partial class frmMain : Form
+    {
+        string[] PDFfiles;
+        int fileCount = 0;
+        string selected_path;
+        public bool ischecked = false;
+        public frmMain() => InitializeComponent();
+        public static int SearchDirectoryTree(string path, out string[] PDFfiles)
+        {
+            PDFfiles = Directory.GetFiles(path, "*.pdf", SearchOption.AllDirectories);
+            return PDFfiles.Length;
+        }
+        public void CropPDF(string srcPdfPath, int x, int y, int width, int height)
+        {
+            // Check if the file exists
+            if (File.Exists(srcPdfPath))
+            {
+                using (PdfReader pdfReader = new PdfReader(srcPdfPath))
+                using (PdfWriter pdfWriter = new PdfWriter("temp2.pdf")) // Write to a temporary file
+                using (PdfDocument pdfDoc = new PdfDocument(pdfReader, pdfWriter))
+                {
+                    if (ischecked)
+                    {
+                        PdfPage firstpage = pdfDoc.GetPage(1); // Get the first page
+
+                        // Define the new crop box
+                        iText.Kernel.Geom.Rectangle cropBox = new iText.Kernel.Geom.Rectangle(x, y, width, height);
+
+                        // Apply the crop box to the page
+                        firstpage.SetCropBox(cropBox);
+                    }
+                    else
+                    {
+                        PdfPage firstpage = pdfDoc.GetPage(1); // Get the first page
+                        PdfPage Secondpage = pdfDoc.GetPage(2); // Get the Second page
+                        PdfPage Thirdpage = pdfDoc.GetPage(3); // Get the Third page
+
+                        // Define the new crop box
+                        iText.Kernel.Geom.Rectangle cropBox = new iText.Kernel.Geom.Rectangle(x, y, width, height);
+                        iText.Kernel.Geom.Rectangle cropBox3 = new iText.Kernel.Geom.Rectangle(0, 0, 590, 1133);
+
+                        // Apply the crop box to the page
+                        firstpage.SetCropBox(cropBox);
+                        Secondpage.SetCropBox(cropBox);
+                        Thirdpage.SetCropBox(cropBox3);
+                    }
+
+                    // Save the document
+                    pdfDoc.Close();
+                }
+                // Replace the original file with the modified file
+                File.Delete(srcPdfPath);
+                File.Move("temp2.pdf", srcPdfPath);
+            }
+        }
+
+        public void DuplicatePage(string inputPdfPath, int pageNbr)
+        {
+            // Check if the file exists
+            if (File.Exists(inputPdfPath))
+            {
+                string outputPdfPath = "temp1.pdf"; // Chemin du fichier PDF de destination
+
+                // Load the original PDF
+                using (PdfReader pdfReader = new PdfReader(inputPdfPath))
+                using (PdfDocument pdfDoc = new PdfDocument(pdfReader))
+                {
+                    // Create a PdfWriter for the new PDF
+                    using (PdfWriter pdfWriter = new PdfWriter(outputPdfPath))
+                    {
+                        // Create a new PdfDocument to write the duplicated content
+                        using (PdfDocument newPdfDoc = new PdfDocument(pdfWriter))
+                        {
+                            // Create a PdfMerger to merge documents
+                            PdfMerger pdfMerger = new PdfMerger(newPdfDoc);
+
+                            // Loop through each page of the original PDF
+                            int numberOfPages = pdfDoc.GetNumberOfPages();
+                            for (int i = 1; i <= numberOfPages; i++)
+                            {
+                                // Merge the current page
+                                pdfMerger.Merge(pdfDoc, i, i);
+                            }
+                            pdfMerger.Merge(pdfDoc, pageNbr, pageNbr);
+                        }
+                    }
+                }
+                // Replace the original file with the modified file
+                File.Delete(inputPdfPath);
+                File.Move(outputPdfPath, inputPdfPath);
+            }
+        }
+        // Button Load ...
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FD = new FolderBrowserDialog();
+            if (selected_path != null)
+                FD.SelectedPath = selected_path;
+
+            if (FD.ShowDialog() == DialogResult.OK)
+            {
+                selected_path = FD.SelectedPath;
+                txtBoxLoad.Text = FD.SelectedPath;
+                fileCount = SearchDirectoryTree(FD.SelectedPath, out PDFfiles);
+                // Check the Empty Folder
+                lblFileCount.Text = fileCount == 0 ? "Your Folder is Empty" : fileCount + " files.";
+                // Clear the Alert message and success message
+                lblDone.Text = "";
+                lblAlert.Text = "";
+                IMGDone.Visible = false;
+            }
+        }
+
+        // Button Start ...
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Clear the Alert message and success message
+            lblDone.Text = "";
+            lblAlert.Text = "";
+            IMGDone.Visible = false;
+
+            // Check if the user has been selected a folder
+            if (PDFfiles == null)
+            {
+                lblAlert.Text = "Please select or Drag your folder and try again!";
+                return;
+            }
+
+            // Check the Empty Folder
+            if (fileCount == 0)
+            {
+                lblAlert.Text = "Your Folder is Empty";
+                return;
+            }
+
+            Cursor = Cursors.WaitCursor;
+
+            try
+            {
+                foreach (string filePath in PDFfiles)
+                {
+                    // Start Convertion ...
+                    // Convert String to Integer 
+                    int x = Convert.ToInt32(txtBoxX.Text);
+                    int y = Convert.ToInt32(txtBoxY.Text);
+                    int width = Convert.ToInt32(txtBoxWidth.Text);
+                    int height = Convert.ToInt32(txtBoxHeight.Text);
+                    // Copy the first page to the last page (page 3)
+                    if (!ischecked)
+                    {
+                        DuplicatePage(filePath, 1);
+                    }
+
+                    // Crop the pages
+                    CropPDF(filePath, x, y, width, height);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Message Exception
+                MessageBox.Show(ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Cursor = Cursors.Default;
+                PDFfiles = null;
+                txtBoxLoad.Text = "Select your folder ...";
+                lblFileCount.Text = "...";
+                return;
+            }
+
+            Cursor = Cursors.Default;
+            lblDone.Text = "Done";
+            IMGDone.Visible = true;
+            PDFfiles = null;
+            txtBoxLoad.Text = "Select your folder ...";
+            lblFileCount.Text = "...";
+        }
+
+        // Start methode : Not Enter a Key String just a Key Number
+        private static void Not_KeyString(KeyPressEventArgs e)
+        {
+            if (e.KeyChar < 48 || e.KeyChar > 57)
+            {
+                // if you press the BACKSPACE key, the Handled property is set to false, 
+                e.Handled = e.KeyChar == 8 ? false : true;
+            }
+        }
+
+        private void txtBoxX_KeyPress(object sender, KeyPressEventArgs e) => Not_KeyString(e);
+
+        private void txtBoxY_KeyPress(object sender, KeyPressEventArgs e) => Not_KeyString(e);
+
+        private void txtBoxHeight_KeyPress(object sender, KeyPressEventArgs e) => Not_KeyString(e);
+
+        private void txtBoxWidth_KeyPress(object sender, KeyPressEventArgs e) => Not_KeyString(e);
+
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            // Condition >> Drag Folder
+            if (Directory.Exists(path))
+            {
+                txtBoxLoad.Text = path;
+                fileCount = SearchDirectoryTree(path, out PDFfiles);
+                selected_path = path;
+                // Check the Empty Folder
+                lblFileCount.Text = fileCount == 0 ? "Your Folder is Empty" : fileCount + " files.";
+            }
+        }
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+            IMGDone.Visible = false;
+            txtBoxLoad.Text = "Select your folder ...";
+            lblDone.Text = "";
+            lblFileCount.Text = "...";
+            lblAlert.Text = "";
+            PDFfiles = null;
+        }
+
+        private void GitLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Go to Github repository
+            string url = "https://github.com/abdessalam-aadel/CropPDFiText7";
+
+            // Open the URL in the default web browser
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true // Ensures the URL is opened in the default web browser
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                ischecked = true;
+                txtBoxX.Text = "793";
+                txtBoxY.Text = "0";
+                txtBoxWidth.Text = "793";
+                txtBoxHeight.Text = "1122";
+                checkBox2.Enabled = false;
+            }
+            else
+            {
+                ischecked = false;
+                // Return default values
+                txtBoxX.Text = "580";
+                txtBoxY.Text = "0";
+                txtBoxWidth.Text = "793";
+                txtBoxHeight.Text = "1122";
+                checkBox2.Enabled = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                ischecked = true;
+                txtBoxX.Text = "580";
+                txtBoxY.Text = "0";
+                txtBoxWidth.Text = "793";
+                txtBoxHeight.Text = "1122";
+                checkBox1.Enabled = false;
+            }
+            else
+            {
+                ischecked = false;
+                // Return default values
+                txtBoxX.Text = "580";
+                txtBoxY.Text = "0";
+                txtBoxWidth.Text = "793";
+                txtBoxHeight.Text = "1122";
+                checkBox1.Enabled = true;
+            }
+        }
+    }
+}
